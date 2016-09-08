5afac5eb66af0cc589e2d881a1e1a870cc9df3bc
Aleksandr Andrienko <aandrienko@codenvy.com>
2016-09-08 19:33:34 +0300

    Support add node validation for 4.0 version

--- wsmaster/codenvy-hosted-license-shared/src/test/java/com/codenvy/api/license/CodenvyLicenseManagerTest.java
+++ wsmaster/codenvy-hosted-license-shared/src/test/java/com/codenvy/api/license/CodenvyLicenseManagerTest.java
@@ -12,7 +12,7 @@
  * is strictly forbidden unless prior written permission is obtained
  * from Codenvy S.A..
  */
-package com.codenvy.api.license.server.license;
+package com.codenvy.api.license;
 
 import org.eclipse.che.commons.lang.IoUtil;
 import org.eclipse.che.commons.lang.NameGenerator;
